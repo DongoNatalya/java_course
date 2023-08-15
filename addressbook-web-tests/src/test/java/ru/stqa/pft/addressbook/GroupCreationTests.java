8f2a13a04efbc9352af36478528eabe5dc0db8d0
DongoNatalya <141217397+DongoNatalya@users.noreply.github.com>
2023-08-16 02:32:17 +0700

    Создан новый модуль addressbook-web-tests Добавлена зависимость от библиотеки Selenium Создан тест для создания новой группы в адресной книге

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,62 +1,51 @@
 package ru.stqa.pft.addressbook;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class GroupCreationTests {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+  private WebDriver wb;
 
-  @BeforeClass(alwaysRun = true)
+  @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "https://www.google.com/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wb = new FirefoxDriver();
+    wb.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
   @Test
-  public void testUntitledTestCase() throws Exception {
-    driver.get("http://localhost/addressbook/index.php");
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.id("LoginForm")).submit();
-    driver.findElement(By.linkText("groups")).click();
-    driver.findElement(By.name("new")).click();
-    driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys("group1");
-    driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys("test1");
-    driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys("test2");
-    driver.findElement(By.name("submit")).click();
-    driver.findElement(By.linkText("group page")).click();
-    driver.findElement(By.linkText("Logout")).click();
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
+  public void testGroupCreation() throws Exception {
+    wb.get("http://localhost/addressbook/index.php");
+    wb.findElement(By.name("user")).clear();
+    wb.findElement(By.name("user")).sendKeys("admin");
+    wb.findElement(By.name("pass")).clear();
+    wb.findElement(By.name("pass")).sendKeys("secret");
+    wb.findElement(By.id("LoginForm")).submit();
+    wb.findElement(By.linkText("groups")).click();
+    wb.findElement(By.name("new")).click();
+    wb.findElement(By.name("group_name")).click();
+    wb.findElement(By.name("group_name")).clear();
+    wb.findElement(By.name("group_name")).sendKeys("group1");
+    wb.findElement(By.name("group_header")).clear();
+    wb.findElement(By.name("group_header")).sendKeys("test1");
+    wb.findElement(By.name("group_footer")).clear();
+    wb.findElement(By.name("group_footer")).sendKeys("test2");
+    wb.findElement(By.name("submit")).click();
+    wb.findElement(By.linkText("group page")).click();
+    wb.findElement(By.linkText("Logout")).click();
+    wb.findElement(By.name("user")).clear();
+    wb.findElement(By.name("user")).sendKeys("admin");
   }
 
-  @AfterClass(alwaysRun = true)
+  @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
+    wb.quit();
   }
 
   private boolean isElementPresent(By by) {
     try {
-      driver.findElement(by);
+      wb.findElement(by);
       return true;
     } catch (NoSuchElementException e) {
       return false;
@@ -65,25 +54,12 @@ public class GroupCreationTests {
 
   private boolean isAlertPresent() {
     try {
-      driver.switchTo().alert();
+      wb.switchTo().alert();
       return true;
     } catch (NoAlertPresentException e) {
       return false;
     }
   }
 
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
+
 }
