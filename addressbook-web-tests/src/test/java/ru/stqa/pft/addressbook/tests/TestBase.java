3012a07a15cba759bab56ef749b9e8d387986499
DongoNatalya <tihonova.natalya@gmail.com>
2023-09-21 00:35:11 +0700

    Лекция 6.9. Конфигурационные файлы для описания тестовых наборов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -6,7 +6,8 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager("chrome");
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", "chrome"));
 
   @BeforeSuite()
   public void setUp() throws Exception {
